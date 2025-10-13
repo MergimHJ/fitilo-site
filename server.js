4febfffc84c845499b57acc76e2f0cc9a1951720
MergimHJ <mergimaskaj@hotmail.com>
2025-10-13 15:31:56 +0200

    fixed request limiter

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ const paymentLimiter = rateLimit({
 
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   standardHeaders: true,
   legacyHeaders: false
 });
